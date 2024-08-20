9e9341096c89adb8999762d90e570e5156e688f1
DefiCake <rarore92@gmail.com>
2024-08-20 20:08:22 +0200

    chore: scaffold portal mock

--- contracts/solidity/FuelMessagePortal.sol
+++ contracts/solidity/FuelMessagePortal.sol
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-/// @dev Emitted when a message is sent from Ethereum to Fuel
-event MessageSent(bytes32 indexed sender, bytes32 indexed recipient, uint256 indexed nonce, uint64 amount, bytes data);
-
-/// @dev Emitted when a message is successfully relayed to Ethereum from Fuel
-event MessageRelayed(bytes32 indexed messageId, bytes32 indexed sender, bytes32 indexed recipient, uint64 amount);
-
 contract FuelMessagePortal {
-    function sendMessageMock(bytes calldata data) public payable {}
+    /// @dev Emitted when a message is sent from Ethereum to Fuel
+    event MessageSent(
+        bytes32 indexed sender, bytes32 indexed recipient, uint256 indexed nonce, uint64 amount, bytes data
+    );
+
+    /// @dev Emitted when a message is successfully relayed to Ethereum from Fuel
+    event MessageRelayed(bytes32 indexed messageId, bytes32 indexed sender, bytes32 indexed recipient, uint64 amount);
+
+    uint256 nonce = 0;
+
+    function sendMessageMock(bytes32 recipient, bytes calldata data) public payable {
+        emit MessageSent(bytes32(bytes20(msg.sender)), recipient, nonce, uint64(msg.value), data);
+        nonce++;
+    }
 }
