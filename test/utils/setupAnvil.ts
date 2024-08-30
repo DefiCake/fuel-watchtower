26804018345a66de331f2ac055b2bdfb0bf4da80
DefiCake <rarore92@gmail.com>
2024-08-30 12:08:59 +0200

    test: make eth test environment hold 20 accounts

--- test/utils/setupAnvil.ts
+++ test/utils/setupAnvil.ts
@@ -2,7 +2,7 @@ import { createAnvil } from '@viem/anvil';
 
 export async function setupAnvil() {
   const port = 49152 + Math.floor(5000 * Math.random());
-  let anvil = createAnvil({ port });
+  let anvil = createAnvil({ port, accounts: 20 });
 
   const rpcUrl = `http://localhost:${anvil.port}`;
 
