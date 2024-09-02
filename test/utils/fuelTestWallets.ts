3b69fd92eb5ad2558406615fe2a7f10e8729fb7c
DefiCake <rarore92@gmail.com>
2024-09-02 18:36:38 +0200

    test: add provider arg to fuelTestWallets

--- test/utils/fuelTestWallets.ts
+++ test/utils/fuelTestWallets.ts
@@ -1,14 +1,16 @@
-import { WalletUnlocked } from 'fuels';
+import { Provider, WalletUnlocked } from 'fuels';
 
 const MNEMONIC = 'test test test test test test test test test test test junk';
 const PATH = "m/44'/1179993420'/0'/0/";
 
-export function fuelTestWallets(nAccounts = 10) {
+export function fuelTestWallets(nAccounts = 10, provider?: Provider) {
   let range = Array.from(Array(nAccounts).keys());
 
   const wallets = range
     .map((n) => PATH + `${n}`)
-    .map((path) => WalletUnlocked.fromMnemonic(MNEMONIC, path));
+    .map((path) =>
+      WalletUnlocked.fromMnemonic(MNEMONIC, path, undefined, provider),
+    );
 
   return wallets;
 }
